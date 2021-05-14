9ba394f5976e9beffa726a014e0f9425792c0eca
YipChiSan <zye39@yahoo.com>
2021-05-14 11:43:20 +1000

    Create some basic getters and setters

--- Patch.java
+++ Patch.java
@@ -1,16 +1,34 @@
 import java.util.ArrayList;
 
 public class Patch {
-    int xAxis;
-    int yAxis;
-    ArrayList<Patch> neighbours;
-    float surfaceAlbedo;
+    private final  int xAxis;
+    private final int yAxis;
+    private ArrayList<Patch> neighbours;
+    Daisy daisy;
+    private final float surfaceAlbedo;
 
     public Patch(int xAxis, int yAxis, List<Patch> neighbours, float surfaceAlbedo) {
         this.xAxis = xAxis;
         this.yAxis = yAxis;
         this.neighbours = neighbours;
         this.surfaceAlbedo = surfaceAlbedo;
-
     }
+
+    public int getXAxis() {
+        return this.xAxis;
+    }
+
+    public int getYAxis() {
+        return this.yAxis;
+    }
+
+    public List<Patch> getNeighbours() {
+        return this.neighbours.clone();
+    }
+
+    public void setDaisy(Daisy daisy) {
+        this.daisy = daisy;
+    }
+
+
 }
