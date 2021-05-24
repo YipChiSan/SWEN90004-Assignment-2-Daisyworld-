05cfd4cd3fc419e7b35ac7dc1de1229b851e726c
Fanyu Meng <fanyu.meng@student.unimelb.edu.au>
2021-05-24 12:07:32 +0800

    Basic input settings

--- Sim.java
+++ Sim.java
@@ -5,7 +5,34 @@ import java.util.concurrent.CountDownLatch;
 public class Sim {
     private Ground ground;
     public static void main(String[] args) {
+        //get all inputs
+        double start_persent_whites = Double.parseDouble(args[0]);
+        double start_persent_black = Double.parseDouble(args[1]);
+        double albedo_of_whites = Double.parseDouble(args[2]);
+        double albedo_of_blacks = Double.parseDouble(args[3]);
 
+        String scenario = "maintain";
+        switch(args[4]){
+            case "ramp":
+                scenario = "ramp";
+                break;
+            case "maintain":
+                scenario = "maintain";
+                break;
+            case "low":
+                scenario = "low";
+                break;
+            case "our":
+                scenario = "our";
+                break;
+            case "high":
+                scenario = "high";
+                break;
+        }
+
+        double solar_luminosity = Double.parseDouble(args[5]);
+        double albedo_of_surface = Double.parseDouble(args[6]);
+        int end_year = Integer.parseInt(args[7]);
     }
 
     
