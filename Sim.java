70d923dd29bfd51db3ee5280793e14f5dba7f58b
YipChiSan <zye39@yahoo.com>
2021-05-26 13:49:21 +1000

    Change the size of ground to 30 * 30

--- Sim.java
+++ Sim.java
@@ -36,7 +36,7 @@ public class Sim {
         if(start_persent_black > 50 || start_persent_whites > 50 || albedo_of_whites >0.99 || albedo_of_blacks > 0.99 || solar_luminosity > 3 || albedo_of_surface > 1){
             System.out.println("invalid input detected.");
         }else{
-            Ground ground = new Ground(40, solar_luminosity, start_persent_whites, start_persent_black, albedo_of_blacks, albedo_of_whites, scenario, albedo_of_surface, end_year);
+            Ground ground = new Ground(30, solar_luminosity, start_persent_whites, start_persent_black, albedo_of_blacks, albedo_of_whites, scenario, albedo_of_surface, end_year);
             ground.run();
         }
 
