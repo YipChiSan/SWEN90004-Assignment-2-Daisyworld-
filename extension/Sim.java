078eb0ea840adb7bc2ff41207bd2a309d8e049dc
YipChiSan <zye39@yahoo.com>
2021-05-27 13:22:53 +1000

    Receive yellow daisy persent as parameter

--- extension/Sim.java
+++ extension/Sim.java
@@ -7,11 +7,13 @@ public class Sim {
 
         double start_persent_whites = Double.parseDouble(args[0]);
         double start_persent_black = Double.parseDouble(args[1]);
-        albedo_of_whites = Double.parseDouble(args[2]);
-        albedo_of_blacks = Double.parseDouble(args[3]);
+        double start_persent_yellows = Double.parseDouble(args[2]);
+        albedo_of_whites = Double.parseDouble(args[3]);
+        albedo_of_blacks = Double.parseDouble(args[4]);
+        
 
         String scenario = "maintain";
-        switch(args[4]){
+        switch(args[5]){
             case "ramp":
                 scenario = "ramp";
                 break;
@@ -29,14 +31,14 @@ public class Sim {
                 break;
         }
 
-        double solar_luminosity = Double.parseDouble(args[5]);
-        double albedo_of_surface = Double.parseDouble(args[6]);
-        int end_year = Integer.parseInt(args[7]);
+        double solar_luminosity = Double.parseDouble(args[6]);
+        double albedo_of_surface = Double.parseDouble(args[7]);
+        int end_year = Integer.parseInt(args[8]);
 
-        if(start_persent_black > 50 || start_persent_whites > 50 || albedo_of_whites >0.99 || albedo_of_blacks > 0.99 || solar_luminosity > 3 || albedo_of_surface > 1){
+        if(start_persent_black + start_persent_whites + start_persent_yellows > 100 || albedo_of_whites >0.99 || albedo_of_blacks > 0.99 || solar_luminosity > 3 || albedo_of_surface > 1){
             System.out.println("invalid input detected.");
         }else{
-            Ground ground = new Ground(30, solar_luminosity, start_persent_whites, start_persent_black, albedo_of_blacks, albedo_of_whites, scenario, albedo_of_surface, end_year);
+            Ground ground = new Ground(30, solar_luminosity, start_persent_whites, start_persent_black, start_persent_yellows, albedo_of_blacks, albedo_of_whites, scenario, albedo_of_surface, end_year);
             ground.run();
         }
 
